c4e9cbfd8a3cf56969cb23626faa26eebc338467
dongwan <zkdlwu94@naver.com>
2021-11-21 16:10:31 +0900

    first commit

---
+++ src/main/java/com/sraw/app/StandardRestApiWebserverApplication.java
@@ -0,0 +1,13 @@
+package com.sraw.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StandardRestApiWebserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StandardRestApiWebserverApplication.class, args);
+	}
+
+}
